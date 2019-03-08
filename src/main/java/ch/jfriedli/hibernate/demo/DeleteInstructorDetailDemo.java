d0b95adf50f3b33deb50664dc3a92dade3909cec
jFriedli <jonas.friedli@hotmail.com>
2019-03-08 15:41:00 +0100

    add ap with cascade delete

---
+++ src/main/java/ch/jfriedli/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,45 @@
+package ch.jfriedli.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ch.jfriedli.hibernate.demo.entity.Instructor;
+import ch.jfriedli.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+			
+			int id = 2;
+			
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+			
+			System.out.println("Delete " + instructorDetail);
+			session.delete(instructorDetail);
+			
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
--- src/main/java/ch/jfriedli/hibernate/demo/entity/InstructorDetail.java
+++ src/main/java/ch/jfriedli/hibernate/demo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package ch.jfriedli.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +23,9 @@ public class InstructorDetail {
 	
 	@Column(name="hobby")
 	private String hobby;
+	
+	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	private Instructor instructor;
 	
 	public InstructorDetail() {};
 	
